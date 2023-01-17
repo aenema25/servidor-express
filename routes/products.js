73a88bb9d8bf985572e4aed37732b4211ffd5dea
Oscar Uribe <oscar@soleit.app>
2023-01-16 23:08:08 -0300

    Añadidas rutas solicitadas en la consigna

--- routes/products.js
+++ routes/products.js
@@ -1,19 +1,49 @@
 const express = require('express');
-const  ProductsClass  = require('../utils/ProductManager');
+const ProductsClass = require('../utils/ProductManager');
 const router = express.Router();
 
 /* GET products. */
-router.get('/products', function(req, res, next) {
+router.get('/products', function (req, res, next) {
   res.send({
     productos: ProductsClass.getProducts().slice(0, req.query.limit)
   })
 });
+/* POST product (create a product). */
+router.post('/products', function (req, res, next) {
+  const addProduct = ProductsClass.addProducts(req.body)
+  if (addProduct.error) {
+    res.status(400).send(addProduct.mensaje)
+  } else {
+    res.status(200).send(addProduct.mensaje)
+  }
+
+});
+/* PUT product (update a product). */
+router.put('/products/:id', function (req, res, next) {
+  const updateProduct = ProductsClass.updateProductById(req.params.id, req.body)
+  if (updateProduct.error) {
+    res.status(400).send(updateProduct.mensaje)
+  } else {
+    res.status(200).send(updateProduct.mensaje)
+  }
+
+});
 /* GET products by id. */
-router.get('/products/:id', function(req, res, next) {
+router.get('/products/:id', function (req, res, next) {
   console.log(req.params.id)
   res.send({
     productos: ProductsClass.getProductById(req.params.id)
   })
+});
+
+/* DELETE products by id. */
+router.delete('/products/:id', function (req, res, next) {
+  const deleteProduct = ProductsClass.deleteProductById(req.params.id)
+  if (deleteProduct.error) {
+    res.status(400).send(deleteProduct.mensaje)
+  } else {
+    res.status(200).send(deleteProduct.mensaje)
+  }
 });
 
 module.exports = router;
